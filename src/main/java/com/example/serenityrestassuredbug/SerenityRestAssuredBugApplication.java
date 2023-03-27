b11b527e72a57cccbd6e943adfd8badaccfae455
Elliot Huntington <elliot.huntington@gmail.com>
2023-03-27 11:24:00 -0600

    Generate spring boot project to demo serenity-rest-assured bug

---
+++ src/main/java/com/example/serenityrestassuredbug/SerenityRestAssuredBugApplication.java
@@ -0,0 +1,13 @@
+package com.example.serenityrestassuredbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerenityRestAssuredBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerenityRestAssuredBugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/serenityrestassuredbug/SerenityRestAssuredBugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serenityrestassuredbug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerenityRestAssuredBugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
